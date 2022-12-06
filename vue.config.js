62712c17bf0d2624bf43b48ca6ce2b7ba3bb1d26
豆豆没有AC <1413314783@qq.com>
2022-12-06 21:26:08 +0800

    更改了配置，修复了gitee pages白屏问题

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? 'ele' : '/',
 
   // vue的反向代理，解决跨域问题，就是让前端的服务器去请求后端的接口
   devServer: {
